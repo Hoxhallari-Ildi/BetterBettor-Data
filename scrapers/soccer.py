55eaad4b831de68f99c7bfc161b499b9c1affc9a
Ildi Hoxhallari <hoxhallari.i@northeastern.edu>
2026-09-26 11:47:22 -0400

    fixes

--- scrapers/soccer.py
+++ scrapers/soccer.py
@@ -10,8 +10,8 @@ from understatapi import UnderstatClient
 # ============================================================
 
 DEFAULT_SEASON = 2026
-DEFAULT_TEAM = "Arsenal"
-DEFAULT_LEAGUE = 'EPL'
+DEFAULT_TEAM = "Paris FC"
+DEFAULT_LEAGUE = 'Ligue_1'
 DEFAULT_N_MATCHES = 3
 DEFAULT_OPPONENT_N_MATCHES = 10
 
